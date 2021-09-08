7def06c995715c1592437cf30fd1efe141eb2d26
Anythingforlife <omprakash.omsai@gmail.com>
2021-09-08 19:00:47 +0530

    role mgmt is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from './component/Login';
 import Registration from './component/Registration';
+import UserComponent from './component/UserComponent';
+import AdminComponent from './component/AdminComponent';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
@@ -9,6 +11,8 @@ const App = () => {
     <BrowserRouter>
       <Route exact path="/" component={Login} />
       <Route path="/Registration" component={Registration} />
+      <Route path="/user" component={UserComponent} />
+      <Route path="/admin" component={AdminComponent} />
     </BrowserRouter>
   );
 };
---
+++ src/component/AdminComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const AdminComponent = () => {
+
+  return (
+    <>
+      <div>
+        <h1>  Admin  page </h1>
+      </div>
+    </>
+  );
+
+};
+
+export default AdminComponent;
