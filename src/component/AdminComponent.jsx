81be31f91b4f00ec909b14b6f37d3c55c3b447cc
omprakash-gomaskar <o.gomaskar@easternenterprise.com>
2021-09-14 11:52:56 +0530

    user table show

--- src/component/AdminComponent.jsx
+++ src/component/AdminComponent.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getData } from '../services/localStorageService';
 const AdminComponent = () => {
+  const key = 'users';
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    const users = getData(key) || [];
+    setUsersData(users.filter(user => user.roleName === 'user'));
+  }, []);
 
   return (
     <>
-      <div>
-        <h1>  Admin  page </h1>
+      <div className="container">
+        <h1>Simple Inventory Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>FirstName</th>
+              <th>Email_ID</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {usersData.map((user, index) =>
+              (<tr key={index}>
+                <td>{user.firstName}</td>
+                <td>{user.email_Id}</td>
+              </tr>)
+            )}
+          </tbody>
+        </table>
       </div>
     </>
   );
--- src/services/localStorageService.js
+++ src/services/localStorageService.js
@@ -1,4 +1,3 @@
 export const getData = (key) => JSON.parse(localStorage.getItem(key));
 
-
 export const setData = (key, value) => localStorage.setItem(key, JSON.stringify(value));
